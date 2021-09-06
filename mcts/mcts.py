eb4eb365a713c03ead7b69cf55bf42cc0ce33cd3
Ariel Perez <arielperezch@gmail.com>
2021-09-06 06:24:23 -0700

    Add some comments and cleanup

--- mcts/mcts.py
+++ mcts/mcts.py
@@ -6,21 +6,21 @@ import numpy as np
 class MCTS:
     """Monte Carlo Tree Search."""
 
-    def __init__(self, evaluation_fn, timeout_ms=1000):
+    def __init__(self, evaluation_fn, timeout_ms=10):
+        """Initialize the search with a given evaluation and timeout."""
         self.counters = {}
         self.evals = {}
         self.evaluation_fn = evaluation_fn
         self.timeout_ms = timeout_ms
 
     def search(self, state):
+        """Perform the search."""
         start_time = time.monotonic()
-        steps = 0
         while (time.monotonic() - start_time) * 1000 < self.timeout_ms:
             chain = self.select(state)
             final_state = self.rollout(chain[-1])
             evaluation = self.evaluation_fn(final_state)
             self.update(chain, evaluation)
-            steps += 1
 
         actions = state.actions()
         possible_states = [state.step(a) for a in actions]
@@ -29,6 +29,7 @@ class MCTS:
         return actions[argmax]
 
     def select(self, state):
+        """Select a state to rollout from."""
         chain = [state]
         current_state = state
         while (current_state in self.counters
@@ -40,6 +41,7 @@ class MCTS:
         return chain
 
     def rollout(self, state):
+        """Play from a state to completion."""
         current_state = state
         while not current_state.is_final:
             actions = current_state.actions()
@@ -48,11 +50,13 @@ class MCTS:
         return current_state
 
     def update(self, states, evaluation):
+        """Update state estimations."""
         for s in states:
             self.counters[s] = self.counters.get(s, 0) + 1
             self.evals[s] = self.evals.get(s, 0) + evaluation
 
     def estimate(self, state):
+        """Compute a state estimation."""
         if state in self.counters:
             return self.evals[state] / self.counters[state]
         return float('-inf')
